92610c86ae4c6b12e0633731a85972afbfc07945
joseph m'bimbi-bene <Joseph.Mbimbi-Bene.ext@ing.com>
2020-05-09 08:22:36 +0200

    fizzbuzz 11 ok ... wtf vim fizzbuzz.asm

--- fizzbuzz.asm
+++ fizzbuzz.asm
@@ -1,9 +1,12 @@
 global fizzbuzz
 extern printf
+extern sprintf
+extern malloc
 
 section .data
-  print       db  "print %d",0xa,0
+  printint    db  "print %d",0xa,0
   printstr    db  "print %s",0xa,0
+  printptr    db  "print %p",0xa,0
   result      db  "result %d",0xa,0
   retvalstr   db  "result str %s",0xa,0
   retval      db  "", 0
@@ -11,6 +14,7 @@ section .data
   fizz        db  "fizz",0
   buzz        db  "buzz",0
   fizzbuzzstr db  "fizzbuzz",0
+  formatint   db  "%d",0
 
 section .text
 
@@ -35,18 +39,32 @@ fizzbuzz:
   cmp edx, 0
   je returnBuzz
 
-  mov eax, [ebp+8]
   jne returnNumber
   
 
+
 returnNumber:
   ; adds input param value to ascii code of "0" -> when not fizz, buzz or fizzbuzz
-  add eax, byte 0x30
-  mov [retval], eax
-  mov eax, retval
+	push    ebx                                     ; 0000 _ 53
+	mov     edi, 3                                  ; 0001 _ BF, 00000003
+	call    malloc                                  ; 0006 _ E8, 00000000(PLT r)
+
+  push dword [ebp+8]
+  push formatint
+  push ebx
+	xor     eax, eax                                ; 001D _ 31. C0
+	call    sprintf                                 ; 001F _ E8, 00000000(PLT r)
+  mov eax, [ebp+8]
+  push eax
+  push printint
+  call printf
+  mov eax, ebx
   mov esp, ebp
   pop ebp
   ret
+
+.LC0:                                                   ; byte
+	db 25H, 64H, 00H                                ; 0000 _ %d.
 
 returnFizz:
   mov eax, fizz
